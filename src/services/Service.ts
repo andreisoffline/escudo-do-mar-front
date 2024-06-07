ae552a846f94ddeeb950c918368e04a6736c6dde
André Prado Junior <116444319+Andrepjr@users.noreply.github.com>
2024-06-07 15:37:29 -0300

    Alteração na Service

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: 'https://escudo-do-mar.onrender.com'
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
